855ccfa718f8a5e4318434303e8e16ea8cb75740
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-08-24 19:07:44 +0900

    dbmodeling3 課題1

--- db_modeling_3/docker-mysql/script/01DDL.sql
+++ db_modeling_3/docker-mysql/script/01DDL.sql
@@ -3,37 +3,3 @@ CREATE TABLE `user` (
   `name` varchar(255),
   `created_at` timestamp DEFAULT (CURRENT_TIMESTAMP)
 );
-
-CREATE TABLE `directory` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int,
-  `name` varchar(255),
-  `created_at` timestamp DEFAULT (CURRENT_TIMESTAMP)
-);
-
-CREATE TABLE `document` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `directory_id` int NOT NULL,
-  `user_id` int,
-  `name` varchar(255),
-  `created_at` timestamp DEFAULT (CURRENT_TIMESTAMP)
-);
-
-CREATE TABLE `directory_relationship` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `parent_id` int,
-  `child_id` int,
-  `created_at` timestamp DEFAULT (CURRENT_TIMESTAMP)
-);
-
-
-ALTER TABLE `directory` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `document` ADD FOREIGN KEY (`directory_id`) REFERENCES `directory` (`id`);
-
-ALTER TABLE `document` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `directory_relationship` ADD FOREIGN KEY (`parent_id`) REFERENCES `directory` (`id`);
-
-ALTER TABLE `directory_relationship` ADD FOREIGN KEY (`child_id`) REFERENCES `directory` (`id`);
-
