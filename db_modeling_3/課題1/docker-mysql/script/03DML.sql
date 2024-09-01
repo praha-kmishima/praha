07f8e668b58050327752bb63fba1336d1e074c80
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-09-01 21:26:10 +0900

    dbmodeling3 課題1 sqlエラー修正

--- db_modeling_3/課題1/docker-mysql/script/03DML.sql
+++ db_modeling_3/課題1/docker-mysql/script/03DML.sql
@@ -55,19 +55,17 @@ DELETE FROM directory_tree WHERE child_id IN (
 -- ・親フォルダと子フォルダa1,a2の親子関係のレコードを削除 ※(4,4)と(5,5)の自己参照レコードは削除しない
 DELETE FROM directory_tree
 WHERE child_id IN (
-  -- id=4を親として持つフォルダのid
   SELECT x.id FROM
     (SELECT child_id AS id
     FROM directory_tree
-    WHERE parent_id = 4) AS X
-) AND parent_id IN (
-  -- id=4を子として持つフォルダのid
+    WHERE parent_id = 4) AS x
+  ) AND parent_id IN (
   SELECT y.id FROM 
     (SELECT parent_id AS id
      FROM directory_tree
      WHERE child_id = 4 
-	    AND parent_id != child_id) AS Y
-	); 
+	    AND parent_id != child_id) AS y
+); 
 
 -- ・移動先フォルダB(id=2)と子孫フォルダa1,a2の関係レコードを追加する
 INSERT INTO directory_tree (parent_id, child_id)
@@ -75,7 +73,7 @@ INSERT INTO directory_tree (parent_id, child_id)
   FROM directory_tree AS super_tree
     CROSS JOIN directory_tree AS sub_tree
    WHERE super_tree.child_id = 2
-     AND sub_tree.parent_id = 4
+     AND sub_tree.parent_id = 4;
      
 -- チェック：フォルダBの子孫を取得
 SELECT d.*
@@ -83,10 +81,10 @@ FROM directory AS d
 	INNER JOIN directory_tree AS t ON d.id = t.child_id
 WHERE t.parent_id = 2;
 
---id;user_id;name;created_at
---2;1;フォルダB;2024-09-01 20:06:50
---4;1;フォルダa1;2024-09-01 20:07:07
---5;1;フォルダa2;2024-09-01 20:07:07
+-- id;user_id;name;created_at
+-- 2;1;フォルダB;2024-09-01 20:06:50
+-- 4;1;フォルダa1;2024-09-01 20:07:07
+-- 5;1;フォルダa2;2024-09-01 20:07:07
 
 -- チェック：フォルダAの子孫を取得
 SELECT d.*
