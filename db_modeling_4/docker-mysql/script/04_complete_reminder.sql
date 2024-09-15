15f3d11712004341431ecacd06925b8a654bed08
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-09-16 00:26:10 +0900

    リマインド完了時のクエリ追加

---
+++ db_modeling_4/docker-mysql/script/04_complete_reminder.sql
@@ -0,0 +1,9 @@
+-- リマインダーが完了した場合の処理
+SET @completed_remind_id = 1;
+
+-- 1. scheduled_remindersから対象idを削除
+DELETE FROM scheduled_reminders WHERE id = @completed_remind_id;
+
+-- 2. completed_remindersに対象idを登録
+INSERT INTO completed_reminders (id, created_at)
+VALUES (@completed_remind_id, NOW());
