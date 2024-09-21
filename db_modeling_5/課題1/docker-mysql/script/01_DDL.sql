3d34f9b6084486525e81b9d454812508b4bc02c5
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-09-21 21:09:39 +0900

    db-modeling5 データベースのDDL作成

---
+++ db_modeling_5/課題1/docker-mysql/script/01_DDL.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL UNIQUE,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `articles` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `history` int NOT NULL DEFAULT 0,
+  `subject` varchar(255) NOT NULL,
+  `content` varchar(3000) NOT NULL,
+  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
