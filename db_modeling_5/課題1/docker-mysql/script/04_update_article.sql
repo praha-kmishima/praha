7ce31456db658710928053f2ab06cd4b132c3ebb
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-09-21 21:34:01 +0900

    db-modeling5 articleのレコードを更新するクエリ作成

---
+++ db_modeling_5/課題1/docker-mysql/script/04_update_article.sql
@@ -0,0 +1,51 @@
+-- hogehogeさんの記事(article_id=1)を改訂する場合のクエリ
+SET @user_id = 1;
+SET @article_id = 1;
+
+-- 修正前のsubject, contentを保存
+SET @subject = NULL;
+SET @content = NULL;
+
+SELECT subject, content
+INTO @subject, @content
+FROM articles
+WHERE id = @article_id;
+
+-- オリジナルの記事レコードを更新（１回目）
+UPDATE articles
+SET 
+  subject = 'ほげほげ（第一回変更）',
+  content = '2024/09/21 初投稿 タイトルを修正しました'
+WHERE id = @article_id;
+
+-- 更新前の記事レコードを挿入
+INSERT INTO articles(user_id, history, subject, content)
+VALUES(
+    @user_id,
+    @article_id,
+    @subject,
+    @content
+);
+
+-- オリジナルの記事レコードを更新（２回目）
+-- 修正前のsubject, contentを保存
+SELECT subject, content
+INTO @subject, @content
+FROM articles
+WHERE id = @article_id;
+
+-- 記事を更新
+UPDATE articles
+SET 
+  subject = 'ほげほげ（第二回変更）',
+  content = '2024/09/21 初投稿 タイトルを修正しました by hogehoge'
+WHERE id = @article_id;
+
+-- 更新前の記事レコードを挿入
+INSERT INTO articles(user_id, history, subject, content)
+VALUES(
+    @user_id,
+    @article_id,
+    @subject,
+    @content
+);
