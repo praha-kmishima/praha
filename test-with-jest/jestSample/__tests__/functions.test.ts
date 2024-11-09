3e7053e07bf5ef705e99d9523768bfe0974fe630
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-11-09 18:02:39 +0900

    asyncSumOfArrayのテストを追加#23

--- test-with-jest/jestSample/__tests__/functions.test.ts
+++ test-with-jest/jestSample/__tests__/functions.test.ts
@@ -1,10 +1,19 @@
-import { sumOfArray } from "../functions";
+import { sumOfArray, asyncSumOfArray } from "../functions";
 
 describe('sumOfArray', () => {
     test('配列内の数値の合計値を返す', () => {
         expect(sumOfArray([1, 2, 3, 4])).toBe(10);
         expect(sumOfArray([0, 0, 0, 0])).toBe(0);
         expect(sumOfArray([-1, -2, -3, -4])).toBe(-10);
+    });
+
+});
+
+describe('asyncSumOfArray', () => {
+    test('配列内の数値の合計値を非同期で返す', async () => {
+        await expect(asyncSumOfArray([1, 2, 3, 4])).resolves.toBe(10);
+        await expect(asyncSumOfArray([0, 0, 0, 0])).resolves.toBe(0);
+        await expect(asyncSumOfArray([-1, -2, -3, -4])).resolves.toBe(-10);
     });
 });
 
