1c563021365ea020c0910f925214ac715f78930b
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-11-09 17:23:20 +0900

    jestによるテストコード実行確認#22

--- test-with-jest/jestSample/__tests__/functions.test.ts
+++ test-with-jest/jestSample/__tests__/functions.test.ts
@@ -1,1 +1,13 @@
-// todo: ここに単体テストを書いてみましょう！
+import { sumOfArray } from "../functions";
+
+describe('sumOfArray', () => {
+    test('配列内の数値の合計値を返す', () => {
+        expect(sumOfArray([1, 2, 3, 4])).toBe(10);
+        expect(sumOfArray([0, 0, 0, 0])).toBe(0);
+        expect(sumOfArray([-1, -2, -3, -4])).toBe(-10);
+    });
+
+});
+
+
+
