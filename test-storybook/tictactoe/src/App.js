f0121680969ab66d1ece5e079a77a05c2eb35697
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-12-04 03:23:54 +0900

    Squareクリック時にハンドラを呼び出す#25

--- test-storybook/tictactoe/src/App.js
+++ test-storybook/tictactoe/src/App.js
@@ -1,5 +1,8 @@
 function Square({ value}) {
-  return <button className="square">{value}</button>;
+  function handleClick() {
+    console.log('click');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>;
 }
 
 export default function Board() {
