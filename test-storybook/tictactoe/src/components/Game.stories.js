575d0f6b0da261d1bb819f7bee069e4b92d194f7
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-12-05 00:45:18 +0900

    playFunctionを使って実際にGameするテストを作成#26

--- test-storybook/tictactoe/src/components/Game.stories.js
+++ test-storybook/tictactoe/src/components/Game.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { within, userEvent } from '@storybook/test';
 import Game from './Game';
 import '../styles.css';
 
@@ -10,3 +11,35 @@ export default {
 const Template = () => <Game />;
 
 export const Default = Template.bind({});
+
+Default.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+  
+    await userEvent.click(canvas.getAllByRole('button')[0], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[1], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[2], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[3], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[4], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[5], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[6], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[7], {
+        delay: 100
+    });  
+    await userEvent.click(canvas.getAllByRole('button')[8], {
+        delay: 100
+    });  
+  };
