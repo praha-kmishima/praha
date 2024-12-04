cf0db6f43400102cc213580574c1e921a80b0246
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-12-04 23:05:28 +0900

    勝利判定を追加#25

--- test-storybook/tictactoe/src/App.js
+++ test-storybook/tictactoe/src/App.js
@@ -11,9 +11,16 @@ function Square({ value, onClick }) {
 export default function Board() {
   const [xisNext, setXisNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = "Next Player: " + (xisNext ? 'X' : 'O');
+  }
 
   function handleClick(i) {
-    if(squares[i]) {
+    if(squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -28,7 +35,8 @@ export default function Board() {
   }
 
   return (
-    <>
+    <> 
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onClick={() => handleClick(0)}/>
         <Square value={squares[1]} onClick={() => handleClick(1)} />
@@ -46,4 +54,23 @@ export default function Board() {
       </div>
     </>
 )
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i]
+    if (squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
 }
