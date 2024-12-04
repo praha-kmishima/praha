77388152b32e60508c976eac2d90c7f39d7494b4
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-12-05 00:23:20 +0900

    Gameのstoryを追加#26

---
+++ test-storybook/tictactoe/src/components/Game.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './Game';
+import '../styles.css';
+
+export default {
+    component: Game,
+    title: 'Game',
+};
+
+const Template = () => <Game />;
+
+export const Default = Template.bind({});
