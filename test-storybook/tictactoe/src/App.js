6ed5d48e155978e50261662056af83e516c2f4eb
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-12-04 22:57:07 +0900

    親のBoardコンポーネントで盤面の状態を管理する#25

--- test-storybook/tictactoe/src/App.js
+++ test-storybook/tictactoe/src/App.js
@@ -1,30 +1,38 @@
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState("");
-  function handleClick() {
-    setValue('X');
-  }
-  return <button className="square" onClick={handleClick}>{value}</button>;
+function Square({ value, onClick }) {
+  return (
+  <button className="square" onClick={onClick}>
+    {value}
+    </button>
+  );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = 'X';
+    setSquares(nextSquares)
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </>
 )
