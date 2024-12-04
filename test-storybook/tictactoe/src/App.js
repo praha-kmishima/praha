f7e3e5e7095560bccd7535e534eb56d362f58244
三島 <k.mishima@fukuyamaconsul.co.jp>
2024-12-04 23:00:12 +0900

    同じマスは書き込めなくする条件追加#25

--- test-storybook/tictactoe/src/App.js
+++ test-storybook/tictactoe/src/App.js
@@ -13,6 +13,10 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if(squares[i]) {
+      return;
+    }
+
     const nextSquares = squares.slice();
     if (xisNext){
       nextSquares[i] = 'X';
