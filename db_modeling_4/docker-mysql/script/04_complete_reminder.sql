2283cbfb8727ac65b67a4bac28c6f045b75d586a
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-01-14 22:24:52 +0900

    関連ファイルは削除

--- db_modeling_4/docker-mysql/script/04_complete_reminder.sql
+++
@@ -1,9 +0,0 @@
--- リマインダーが完了した場合の処理
-SET @completed_remind_id = 2;
-
--- 1. scheduled_remindersから対象idを削除
-DELETE FROM scheduled_reminders WHERE reminder_id = @completed_remind_id;
-
--- 2. completed_remindersに対象idを登録
-INSERT INTO completed_reminders (reminder_id, created_at)
-VALUES (@completed_remind_id, NOW());
